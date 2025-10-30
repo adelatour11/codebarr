04bc01ca74603bdc05cb4a6beb31b92004e22e9c
Hidea <adelatour11@yahoo.fr>
2025-10-30 15:35:35 +0100

    Create service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  console.log('📦 Service Worker installing...');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('🚀 Service Worker active.');
+});
+
+self.addEventListener('fetch', event => {
+});
